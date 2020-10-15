01c7a70f5de0980958349d4b41a6ec7732ae4440
hiagopsilva <hiagohps360@gmail.com>
2020-10-14 23:57:23 -0300

    updated

---
+++ server/src/server.ts
@@ -0,0 +1,23 @@
+import express from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
+import path from 'path'
+import 'express-async-errors'
+
+import './database/connection'
+
+import routes from './routes'
+import errorHandler from './errors/hadler'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(morgan('dev'))
+
+app.use(routes)
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
+app.use(errorHandler)
+app.listen(3333, () => {
+  console.log('Server started in port: 3333')
+})
